7f232a5d9e72a948eb2fa9c802374b1cdc852b2d
akash3252 <akashta0700@gmail.com>
2025-11-04 21:27:29 +0530

    Delete colorlist.py

--- colorlist.py
+++
@@ -1,7 +0,0 @@
-color_list1=input("enter color for list 1(comma seperated: )").split(',')
-print(color_list1)
-color_list2=input("enter color for list 2(comma seperated: )").split(',')
-print(color_list2)
-unique_color=[color for color in color_list1 if color not in color_list2]
-print("colors from colorlist one that not in list 2:")
-print(unique_color)

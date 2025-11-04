bca41d2894113ab918b62ad32b1987bea8fdc332
akash3252 <akashta0700@gmail.com>
2025-11-04 20:53:50 +0530

    Delete returnlenth.py

--- returnlenth.py
+++
@@ -1,8 +0,0 @@
-list=input("enter a list of words seperated by spaces").split(" ")
-longest_length=0
-for word in list:
-    if len(word)>longest_length:
-        longest_length=len(word)
-        long_word=word
-print("The longest word is",long_word)
-print("the length of the longest word is",longest_length)
